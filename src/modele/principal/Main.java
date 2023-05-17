b40a720d37f06400b1557ff1516e35b3cec0cc73
Benjamin Nicol <benjamin.nicol@iut-rodez.fr>
2023-05-17 15:11:47 +0200

    Initialisation

---
+++ src/modele/principal/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Main.java                                      17 mai 2023
+ * IUT Rodez, info1 2022-2023, pas de copyright ni "copyleft" 
+ */
+package modele.principal;
+
+/** TODO comment class responsibility (SRP)
+ * @author benjamin.nicol
+ *
+ */
+public class Main {
+
+    /** TODO comment method role
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/modele/test/testMain.java
@@ -0,0 +1,38 @@
+/*
+ * testMain.java                                      17 mai 2023
+ * IUT Rodez, info1 2022-2023, pas de copyright ni "copyleft" 
+ */
+package modele.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/** TODO comment class responsibility (SRP)
+ * @author benjamin.nicol
+ *
+ */
+class testMain {
+
+    /** TODO comment method role
+     * @throws java.lang.Exception
+     */
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    /** TODO comment method role
+     * @throws java.lang.Exception
+     */
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
