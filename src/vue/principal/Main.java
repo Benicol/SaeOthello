741f1603c05f393de24187c66809792c5f557bb0
Jodie Monterde <jodie.monterde@iut-rodez.fr>
2023-05-26 08:32:59 +0200

    - Ajout des modèles de vue créées sur SceneBuilder 	--> L'interface de jeu 	--> Le menu principal - Modification du path pour avoir accès à JavaFx

--- src/vue/principal/Main.java
+++ src/vue/principal/Main.java
@@ -1,25 +1,35 @@
-/*
- * Main.java                                      17 mai 2023
- * IUT Rodez, info1 2022-2023, pas de copyright ni "copyleft" 
- */
 package vue.principal;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
-/**
- * TODO comment class responsibility (SRP)
- * 
- * @author benjamin.nicol
+
+/** TODO comment class responsibility (SRP)
+ * @author Jodie Monterde
  *
  */
-public class Main {
-
-    /**
-     * TODO comment method role
-     * 
-     * @param args
-     */
-    public static void main(String[] args) {
-       System.out.print("salut");
-
-    }
-
+public class Main extends Application {
+	/* non javadoc - @see javafx.application.Application#start(javafx.stage.Stage) */
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,600,700);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			/** */
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/** TODO comment method role
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
